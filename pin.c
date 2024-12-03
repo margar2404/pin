e6aa9f7dc3817cee8b44f315aebf41c00968fa01
margar2404 <mgarridocolom@gmail.com>
2024-12-03 13:36:53 +0100

    Add files via upload

---
+++ pin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main (){
+int resposta;
+int errades;
+errades = 0;
+do{
+
+printf("Pin?: ");
+    scanf("%d", &resposta);
+if(resposta!=1234) errades++;
+}while((resposta!=1234)&&(errades<3));
+if(resposta!=1234)
+printf("Ok!\n");
+else printf("Sistema bloquejat!\n")
+return;
+}
